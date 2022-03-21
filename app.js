075e2563b7c63116d387482029387c1a93e2cc8e
emrah dautbegovic <emrahdautbegovic@emrahs-MacBook-Pro.local>
2022-03-21 15:27:55 +0100

    Initial version v1.0.0

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const session  = require('express-session');
+const connectDB = require('./config/db')
+
+const app = express();
+const PORT = 3000;
+
+//passport config
+require('./config/passport')(passport);
+
+//load dotenv config.
+dotenv.config();    
+connectDB();
+//handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main' , 'extname' : '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Express sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes
+app.use('/',require('./routes/index'));
+app.use('/auth',require('./routes/auth'));
+
+
+app.listen(PORT , console.log('LIstening at port 3000'));
